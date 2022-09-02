beeae99a5d3ee7514cf8fdb5d54a4c2b62001844
Federico Verly <federicoverly@gmail.com>
2022-09-02 17:44:14 +0200

    full_commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { useEffect } from 'react'
-import './App.css';
-import AboutMe from './AboutMe';
-import Contact from './Contact';
-import Header from './Header';
-import Navbar from './Navbar';
-import Portfolio from './Portfolio';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-
+import { useEffect } from "react";
+import "./App.css";
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Portfolio from "./Portfolio";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-
   useEffect(() => {
-    Aos.init({ duration: 1000})
-  }, [])
-  
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
-        
-        <div className="app">
+    <div className="app">
+      <Navbar />
 
-             <Navbar />
+      {/* Header */}
+      <Header />
 
-            {/* Header */}
-             <Header />
+      {/* Portfolio */}
+      <Portfolio />
 
-            {/* Portfolio */}
-              <Portfolio />
-            {/* About */}
-              <AboutMe />
+      {/* About */}
+      <AboutMe />
 
-            {/* Contact */}
-              <Contact />
-
-         </div>
+      {/* Contact */}
+      <Contact />
+    </div>
   );
 }
 
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,89 +1,104 @@
-import React, { useEffect } from 'react';
-import './Portfolio.css';
+import React, { useEffect } from "react";
+import "./Portfolio.css";
 
 // Animation
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-
-// Videos import
-import facebook from './1._Facebook_Clone.mp4';
-import ecommerce from './2._Ecommerce.mp4';
-import instagram from './3._Instagram_Clone.mp4';
-import netflix from './4._Netflix_Clone.mp4';
-import spotify from './5._Spotify_Clone.mp4';
-import discord from './6._Discord_Clone.mp4';
-import landing1 from './7._Del_Arroyo.mp4';
-import tinder from './8._Tinder_Clone.mp4'
-import twitch from './9._Twitch_Clone.mp4';
-import whatsapp from './10._Whatsapp_Clone.mp4'
-import landing2 from './11._Restaurant.mp4';
-import blog from './12._Blog.mp4';
-
-
-const videos = [ {
+const videos = [
+  {
     title: "Facebook Clone",
-    video: facebook,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/1._Facebook_Clone.mp4?alt=media&token=1b84f6b0-f19e-4896-bfe3-f1fb1e410c76",
+  },
+  {
     title: "Ecommerce",
-    video: ecommerce,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/2._Ecommerce.mp4?alt=media&token=601ab01d-bb8e-4840-b1d8-ab3f342d1de5",
+  },
+  {
     title: "Instagram Clone",
-    video: instagram,
-},{
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/3._Instagram_Clone.mp4?alt=media&token=692d36ca-0bd2-4b64-91a0-01cb169f7be8",
+  },
+  {
     title: "Netflix Clone",
-    video: netflix,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/4._Netflix_Clone.mp4?alt=media&token=9a0c5a26-6a05-4290-b78c-90be368e3a80",
+  },
+  {
     title: "Spotify Clone",
-    video: spotify,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/5._Spotify_Clone.mp4?alt=media&token=3dba37d3-3539-4849-9b69-1f33ead62556",
+  },
+  {
     title: "Discord Clone",
-    video: discord,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/6._Discord_Clone.mp4?alt=media&token=88a545bc-3cde-4467-92e9-2c23b7eb1724",
+  },
+  {
     title: "Tinder Clone",
-    video: tinder,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/8._Tinder_Clone.mp4?alt=media&token=0e103ea9-7f54-459f-ab10-04c92c7cd40d",
+  },
+  {
     title: "Twitch Clone",
-    video: twitch,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/9._Twitch_Clone.mp4?alt=media&token=cee88688-779b-4d68-bfe6-eb2ce1cedb59",
+  },
+  {
     title: "Whatsapp Clone",
-    video: whatsapp,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/10._Whatsapp_Clone.mp4?alt=media&token=fc13597e-28f3-4c32-824f-1f360a72dfbf",
+  },
+  {
     title: "Blog",
-    video: blog,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/12._Blog.mp4?alt=media&token=54b6706e-dacb-4bb2-b971-4c81ed2a0052",
+  },
+  {
     title: "Landing Example 1",
-    video: landing1,
-}, {
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/11._Restaurant.mp4?alt=media&token=12aaf3a7-cf46-4c9b-bc2e-3e9eaecadb33",
+  },
+  {
     title: "Landing Example 2",
-    video: landing2,
-}]
-
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/7._Del_Arroyo.mp4?alt=media&token=b2feda22-3b97-44b8-b5b1-2db7fa5ef90a",
+  },
+];
 
 function Portfolio() {
-    useEffect(() => {
-        Aos.init({ duration: 2000})
-      }, [])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
-    return (
-        <>
-        <div className="portfolio"  >   
-        <h2  id="portfolio">HIDDEN</h2>        
-            <div data-aos="fade-up" className="portfolio__title" >
-                <h1>PORTFOLIO</h1>
-            </div>
-            <div  data-aos="fade-up" className="portfolio__videos">
-                {videos.map( video => (
-                    <div className="video">
-                        <h3>{video.title}</h3>
-                        <video src={video.video} width="500" height="250" controls="controls" autoplay="true" className="video__small"/>
-                    </div>
-                    ))}
-                
-            </div>
+  return (
+    <>
+      <div className="portfolio">
+        <h2 id="portfolio">HIDDEN</h2>
+        <div data-aos="fade-up" className="portfolio__title">
+          <h1>PORTFOLIO</h1>
         </div>
-        </>
-    )
+        <div data-aos="fade-up" className="portfolio__videos">
+          {videos.map((video) => (
+            <div className="video">
+              <h3>{video.title}</h3>
+              <iframe
+                src={video.video}
+                width="500"
+                height="250"
+                controls="controls"
+                autoplay="true"
+                className="video__small"
+                title={video.title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
